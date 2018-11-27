1a002bc2d42d354d18598efd5854b86a907a2754
Sukjune Hong <sukjune.hong@gmail.com>
2018-11-27 00:34:01 -0500

    mLab

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const MONGODB_URI = require("./config/keys")
+const MONGODB_URI = require("./config/keys.js")
 
 const PORT = PORT || 8080;
 const app = express();
